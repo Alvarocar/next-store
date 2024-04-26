6a86b062170328bb01e7519625f28693964224e7
Alvaro Cardozo <alvaro_car6@hotmail.com>
2024-04-25 22:55:11 -0500

    refactor: change global styles

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,6 @@ const __dirname = dirname(__filename);
 const nextConfig = {
   sassOptions: {
     includePaths: [join(__dirname, "src/sass")],
-    prependData: '@import "main";',
   },
   images: {
     remotePatterns: [
